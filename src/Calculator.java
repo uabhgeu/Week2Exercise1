9782174ce3b4633822e4b3b6825a6aaa9ce59a2b
Henrik Gustafsson <henrik.gustafsson@consid.se>
2020-03-26 15:52:39 +0100

    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+
+public class Calculator {
+	public int addition(int num1, int num2) {
+		int addResult = (num1 + num2);
+		return addResult;
+	}
+	
+	public double addition(double num1, double num2) {
+		double addResult = (num1 + num2);
+		return addResult;
+	}
+	
+	public int subtraction(int num1, int num2) {
+		int subResult = (num1 - num2);
+		return subResult;
+	}
+	
+	public double subtraction(double num1, double num2) {
+		double subResult = (num1 - num2);
+		return subResult;
+	}
+	
+	public long addition(long num1, long num2) {
+		long addResult = (num1 + num2);
+		return addResult;
+	}
+		
+}
+
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator result = new Calculator();
+//		double tal1 = 5;
+//		double tal2 = 3;
+		System.out.println("Result of NrA + NrB = " + result.addition(8.5,8.5));
+		System.out.println("Result of NrA - NrB = " + result.subtraction(5.8,5.5));
+
+	}
+
+}
